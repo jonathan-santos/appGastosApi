50a1cb112251059cd2efc58fc1af6c31fadf8cbf
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-09-11 08:59:03 -0300

    Atualizado  a URL da chamada NovoGasto no script Novo

--- novo.js
+++ novo.js
@@ -5,7 +5,7 @@ const novoGasto = async (gasto) => {
 
     try {
         console.log("novoGasto: " + JSON.stringify(gasto))
-        await api.post("values/janeiro!H3:M3:append", {
+        await api.post("values/01%2F2019!A5:F5:append", {
             values: [[
                 gasto.data,
                 gasto.titulo,
